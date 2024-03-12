8e43709a654bb761b2f9934f4b8cd469aa7470eb
Árnilsen Arthur Castilho Lopes <arnilsenarthur@gmail.com>
2024-03-11 23:32:26 -0300

    Initial Ideias

---
+++ Assets/Scripts/Circuit/CircuitButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace SolarBuff.Circuit
+{
+    public class CircuitButton : CircuitComponent
+    {
+        public CircuitPlug output;
+        
+        public override IEnumerable<CircuitPlug> GetPlugs()
+        {
+            yield return output;
+        }
+    }
+}
---
+++ Assets/Scripts/Circuit/CircuitLamp.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace SolarBuff.Circuit
+{
+    public class CircuitLamp : CircuitComponent
+    {
+        public CircuitPlug input;
+        
+        public override IEnumerable<CircuitPlug> GetPlugs()
+        {
+            yield return input;
+        }
+    }
+}
