9c51047e094ef5e3b4cf1c607794bce3b78405e1
Arnilsen Arthur <arnilsenarthur@gmail.com>
2024-03-19 17:03:36 -0300

    Test
    Test

---
+++ Assets/Scripts/LatencyTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NetBuff.Components;
+using UnityEngine;
+
+namespace SolarBuff
+{
+    public class LatencyTest : NetworkBehaviour
+    {
+        public Vector3 pointA = new(5, 0, 5);
+        public Vector3 pointB = new(-5, 0, 5);
+        
+        public void Update()
+        {
+            if (!HasAuthority)
+                return;
+
+            transform.position = Vector3.Lerp(pointA, pointB, Mathf.PingPong(Time.time, 1));
+        }
+    }
+}
