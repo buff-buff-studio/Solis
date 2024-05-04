02cdcde700809466dd2179dc8c9c915eef36cfed
Arnilsen Arthur <arnilsenarthur@gmail.com>
2024-05-04 15:15:23 -0300

    Level Exit

---
+++ Assets/Scripts/Misc/Props/LevelExit.cs
@@ -0,0 +1,36 @@
+﻿using NetBuff;
+using Solis.Core;
+using Solis.Player;
+using UnityEngine;
+
+namespace Solis.Misc.Props
+{
+    /// <summary>
+    /// When the player collides with this object, the level will be exited
+    /// </summary>
+    public class LevelExit : MonoBehaviour
+    {
+        #region Unity Callbacks
+        private void OnTriggerEnter(Collider other)
+        {
+            var controller = other.GetComponent<PlayerControllerBase>();
+            if (controller == null)
+                return;
+            
+            if(NetworkManager.Instance == null)
+                return;
+
+            if (!NetworkManager.Instance.IsServerRunning)
+                return;
+
+            var game = GameManager.Instance;
+            
+            if (game == null)
+                return;
+
+            game.SaveData.currentLevel++;
+            game.LoadLevel();
+        }
+        #endregion
+    }
+}
