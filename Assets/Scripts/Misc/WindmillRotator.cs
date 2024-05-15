f7a618ec9b9d748d479f3cc94e37310a1eceaeb0
Arnilsen Arthur <arnilsenarthur@gmail.com>
2024-05-15 15:26:43 -0300

    Controller Fixes + Windmills

---
+++ Assets/Scripts/Misc/WindmillRotator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Solis.Misc
+{
+    /// <summary>
+    /// Class that rotates the windmill blades
+    /// </summary>
+    public class WindmillRotator : MonoBehaviour
+    {
+        public float speed = 10f;
+        
+        void Update()
+        {
+            transform.Rotate(Vector3.right, speed * Time.deltaTime);
+        }
+    }
+}
