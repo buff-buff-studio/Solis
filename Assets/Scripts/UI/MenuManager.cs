1818ff8f0b5f318eb113b1b3e26f03902ea29d75
Sérgio Macellani <sergiododec@gmail.com>
2024-06-08 22:37:40 -0300

    Fix: components without network transform

--- Assets/Scripts/UI/MenuManager.cs
+++ Assets/Scripts/UI/MenuManager.cs
@@ -1,11 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 namespace Solis.UI
 {
     public class MenuManager : WindowManager
     {
+        public TextMeshProUGUI versionText;
+
+        private void Awake()
+        {
+            versionText.text = $"V: {Application.version}";
+        }
+
         public void ExitGame()
         {
 #if UNITY_EDITOR
@@ -14,5 +23,12 @@ namespace Solis.UI
         Application.Quit();
 #endif
         }
+#if UNITY_EDITOR
+        protected override void OnValidate()
+        {
+            if (versionText != null) versionText.text = $"V: {Application.version}";
+            base.OnValidate();
+        }
+#endif
     }
 }
