02da7480b9a5ff18a5c28c0c34a726e90068cc2a
Arnilsen Arthur <arnilsenarthur@gmail.com>
2024-06-14 18:08:39 -0300

    Scripts

---
+++ Assets/Scripts/Relay/RelayNetworkManagerGUI.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace NetBuff.Relays
+{
+    public class RelayNetworkManagerGUI : MonoBehaviour
+    {
+        public string code = "";
+        private void OnGUI()
+        {
+            var rnm = (RelayNetworkManager) NetworkManager.Instance;
+            if (rnm.Transport.Type == NetworkTransport.EnvironmentType.None)
+            {
+                code = GUI.TextField(new Rect(10, 10, 200, 20), code);
+
+                if (GUI.Button(new Rect(10, 40, 200, 20), "Join"))
+                {
+                    Debug.Log("Joining with code: " + code);
+                
+                    rnm.JoinRelayServer(code, (success) =>
+                    {
+                        Debug.Log("Success: " + success);
+                    });
+                }
+
+                if (GUI.Button(new Rect(10, 70, 200, 20), "Create"))
+                {
+                    Debug.Log("Creating");
+                    rnm.StartRelayHost(4, "", (success, c) =>
+                    {
+                        this.code = c;
+                        Debug.Log("Success: " + success + " Code: " + code);
+                    });
+                }
+            }
+            else
+            {
+                GUI.Label(new Rect(10, 10, 200, 20), "Room: " + code);
+                if (GUI.Button(new Rect(10, 40, 200, 20), "Close"))
+                {
+                    rnm.Close();
+                }
+            }
+        }
+    }
+}
