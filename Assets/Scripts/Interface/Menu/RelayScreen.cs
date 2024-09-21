494b38df0090406aa7df277678322fd79311bbc6
Sérgio Macellani <sergiododec@gmail.com>
2024-09-21 19:11:10 -0300

    Menu things

--- Assets/Scripts/Interface/Menu/RelayScreen.cs
+++ Assets/Scripts/Interface/Menu/RelayScreen.cs
@@ -13,14 +13,36 @@ namespace Interface
         #region Inspector Fields
         [Header("REFERENCES")]
         public TMP_InputField inputFieldUsername;
+        public TMP_Text textLoggedIn;
         public TMP_InputField inputRelayCode;
         #endregion
 
         private async void OnEnable()
         {
+#if PLATFORM_STANDALONE_WIN
+            inputFieldUsername.text = System.Environment.UserName;
+#else
+            inputFieldUsername.text = SystemInfo.deviceName;
+#endif
+            if(string.IsNullOrEmpty(inputFieldUsername.text) || string.IsNullOrWhiteSpace(inputFieldUsername.text) || inputFieldUsername.text == "<unknown>")
+                inputFieldUsername.text = $"Player {UnityEngine.Random.Range(0, 1000)}";
+
+            //resize the input field to fit the text
+            ResizeUsername();
+            //resize the logged in text to fit the text
+            textLoggedIn.ForceMeshUpdate();
+            textLoggedIn.rectTransform.sizeDelta = new Vector2(textLoggedIn.preferredWidth, textLoggedIn.rectTransform.sizeDelta.y);
+
             await UnityServices.InitializeAsync();
             if (!AuthenticationService.Instance.IsSignedIn)
                 await AuthenticationService.Instance.SignInAnonymouslyAsync();
+        }
+
+        public void ResizeUsername()
+        {
+            inputFieldUsername.ForceLabelUpdate();
+            inputFieldUsername.GetComponent<RectTransform>().sizeDelta =
+                new Vector2(inputFieldUsername.preferredWidth, inputFieldUsername.GetComponent<RectTransform>().sizeDelta.y);
         }
 
         public void Create()
